2beff04d1071fd6b28f001ee123773438a0ca28b
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-04-16 12:29:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stream First Non-repeating/stream-first-nonrepeating.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String A = br.readLine().trim();
+            Solution ob = new Solution();
+            String ans = ob.FirstNonRepeating(A);
+            out.println(ans);
+
+            out.println("~");
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public String FirstNonRepeating(String s) {
+        // code here
+        StringBuilder sb = new StringBuilder();
+        int[] frequency = new int[26];
+        Queue<Character> q=new LinkedList<>();
+        
+        for(int i=0;i<s.length();i++){
+            char c =s.charAt(i);
+            
+            q.offer(c);
+            frequency[c-'a']++;
+            
+            while(!q.isEmpty() && frequency[q.peek()-'a']>1){
+                q.poll();
+            }
+            
+            if(q.isEmpty()){
+                sb.append("#");
+            }else{
+                sb.append(q.peek());
+            }
+        }
+        return sb.toString();
+    }
+}
