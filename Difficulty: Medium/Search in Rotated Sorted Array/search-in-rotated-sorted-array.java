bfb83b96802f6f59f189c1d2f66d22b63306f954
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-04-26 13:41:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
@@ -41,13 +41,35 @@ public class GFG {
 // User function Template for Java
 
 class Solution {
-    int search(int[] arr, int key) {
-        // Complete this function
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==key){
-                return i;
-            }
+    int search(int[] nums, int target) {
+       int l = 0;
+    int r = nums.length - 1;
+    int result = -1;
+
+    while (l <= r) {
+      int m = (l + r) >>> 1;
+
+      if (target == nums[m]) {
+        result = m;      // store the index
+        r = m - 1;       // continue searching on the left side
+      } else if (nums[m] == nums[l] && nums[m] == nums[r]) {
+        l++;
+        r--;
+      } else if (nums[l] <= nums[m]) {
+        if (nums[l] <= target && target < nums[m]) {
+          r = m - 1;
+        } else {
+          l = m + 1;
         }
-        return -1;
+      } else {
+        if (nums[m] < target && target <= nums[r]) {
+          l = m + 1;
+        } else {
+          r = m - 1;
+        }
+      }
+    }
+
+    return result;
     }
 }
