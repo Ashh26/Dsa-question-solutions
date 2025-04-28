14e3e9f6e562b7de372d399bcc332ae03c7a77cb
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-04-28 14:43:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -38,66 +38,66 @@ class Main {
 
 
 class Solution {
-    int[] array;
-    int[] tempArray;
-    int length;
+public void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-    void mergeSort(int arr[], int l, int r) {
-        this.array = arr;
-        this.length = arr.length;
-        this.tempArray = new int[length];
-        
-        divideArray(array,0,length-1);
-    }
-    
-    public void divideArray(int[] array,int l,int r)
-    {
-        if(l<r){
-            int m = (l+r)/2;
-            
-            divideArray(array,l,m);
-            
-            divideArray(array,m+1,r);
-            
-            mergeArray(l,m,r);
-        }
-    }
-    
-    public void mergeArray(int l,int m,int r)
-    {
-        for(int i=l;i<=r;i++){
-            tempArray[i] = array[i];
-        }
-        
-        int i=l;
-        int j=m+1;
-        int k=l;
-        
-        while(i<=m && j<=r){
-            if(tempArray[i]<=tempArray[j]){
-                array[k] = tempArray[i];
+        // Create temporary arrays
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            left[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            right[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+
+        int i = 0, j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
                 i++;
-            }else{
-                array[k] = tempArray[j];
+            }
+            else {
+                arr[k] = right[j];
                 j++;
             }
             k++;
         }
-        
-        while(i<=m){
-            array[k] = tempArray[i];
-            k++;
+
+        // Copy remaining elements of left[], if any
+        while (i < n1) {
+            arr[k] = left[i];
             i++;
+            k++;
         }
-        
+
+        // Copy remaining elements of right[], if any
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    public void mergeSort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
     }
 }
-
-
-
-
-
-
-
-
-
