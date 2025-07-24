da4e5fc0c39bbad00d12398992b5ac8be47913fc
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-07-24 22:25:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Pattern 7/pattern-7.java
+++ Difficulty: Basic/Pattern 7/pattern-7.java
@@ -1,38 +1,15 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Main {
-    // Driver code
-    public static void main(String[] args) throws Exception {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            Solution obj = new Solution();
-            obj.printTriangle(n);
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
 
     void printTriangle(int n) {
-        // for pattern 1
-       for(int row=1;row<=n;row++){
-            for(int col=row;col<n;col++){
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
                 System.out.print(" ");
             }
-            for(int col=1;col<=2*row-1;col++){
+            for(int k=1;k<=2*i-1;k++){
                 System.out.print("*");
             }
             System.out.println();
         }
-        
     }
 }
