373366674d80778fe7edafe0a09542bae227a58a
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-11-20 00:06:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -1,60 +1,39 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().majorityElement(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-   static int majorityElement(int arr[]) {
-        int n = arr.length;
+    int majorityElement(int arr[]) {
+       
+       int n = arr.length;
+        int candidate = -1;
+        int count = 0;
 
-        // Step 1: Find the candidate for majority element
-        int candidate = -1, count = 0;
-        
-        for (int i = 0; i < n; i++) {
+        // Find a candidate
+        for (int num : arr) {
             if (count == 0) {
-                candidate = arr[i];
+                candidate = num;
                 count = 1;
-            } else if (arr[i] == candidate) {
+            } 
+            else if (num == candidate) {
                 count++;
-            } else {
+            }
+            else {
                 count--;
             }
         }
 
-        // Step 2: Verify if the candidate is the majority element
+        // Validate the candidate
         count = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == candidate) {
+        for (int num : arr) {
+            if (num == candidate) {
                 count++;
             }
         }
-
-        // If the count is more than n/2, return the candidate
+	
+      	// If count is greater than n / 2, return 
+      	// the candidate; otherwise, return -1
         if (count > n / 2) {
             return candidate;
+        } else {
+            return -1;
         }
-
-        // No majority element found
-        return -1;
+        
     }
 }
