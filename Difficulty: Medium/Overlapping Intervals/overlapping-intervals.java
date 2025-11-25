1ab49958a1a26e50c2fc7e7a7ca0335e56913891
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-11-25 23:32:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
@@ -1,61 +1,24 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            int[][] arr = new int[n][2];
-            for (int i = 0; i < n; i++) {
-                String temp[] = br.readLine().trim().split(" ");
-                arr[i][0] = Integer.parseInt(temp[0]);
-                String x = temp[1];
-                arr[i][1] = Integer.parseInt(x);
-            }
-            Solution obj = new Solution();
-            // The mergeOverlap function now returns a List<int[]>
-            List<int[]> ans = obj.mergeOverlap(arr);
-
-            // Printing the merged arr
-            for (int[] interval : ans) {
-                System.out.print(interval[0] + " " + interval[1] + " ");
-            }
-            System.out.println();
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    public List<int[]> mergeOverlap(int[][] arr) {
+    public ArrayList<int[]> mergeOverlap(int[][] arr) {
+        // Sort intervals based on start values
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
 
-        // sort the array based on start values
-        Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
-        
-        // index of last merged
-        List<int[]> result = new ArrayList<>();
-        result.add(new int[]{arr[0][0],arr[0][1]});
-        
-        for(int i=1;i<arr.length;i++){
-            // if current interval overlaps with the last merged interval
-            int[] last = result.get(result.size()-1);
+        ArrayList<int[]> res = new ArrayList<>();
+        res.add(new int[]{arr[0][0], arr[0][1]});
+
+        for (int i = 1; i < arr.length; i++) {
+            int[] last = res.get(res.size() - 1);
             int[] curr = arr[i];
-            
-            if(curr[0]<=last[1]){
-                last[1] = Math.max(last[1],curr[1]);
-            }else{
-                result.add(new int[]{curr[0],curr[1]});
-            }
-            
-        }
-        
-        // retunr size of the merged intervals
-        return result;
 
+            // If current interval overlaps with the last merged interval,
+            // merge them
+            if (curr[0] <= last[1])
+                last[1] = Math.max(last[1], curr[1]);
+            else
+                res.add(new int[]{curr[0], curr[1]});
+        }
+
+        return res;
+        
     }
 }
