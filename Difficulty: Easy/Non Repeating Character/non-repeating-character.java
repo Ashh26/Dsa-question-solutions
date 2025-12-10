100a1a90e9b2ed017e0b2bc1b8a991a4b99a0978
Yasif khan <125533092+Ashh26@users.noreply.github.com>
2025-12-10 21:06:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -1,62 +1,29 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            String st = sc.next();
-
-            char ans = new Solution().nonRepeatingChar(st);
-
-            if (ans != '$')
-                System.out.println(ans);
-            else
-                System.out.println(-1);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to find the first non-repeating character in a string.
-    static char nonRepeatingChar(String s) {
-        // store the length of the character in n
-        int n = s.length();
+    public char nonRepeatingChar(String s) {
+        // code here
         
-        // first loop for pick one char at one time
-        for(int i=0;i<n;i++){
-            // in every iteration found will be false after second loop is fasle is true then all the character is repeated
-            boolean found = false;
-            
-            // second loop for comparing each character to every character
-            for(int j=0;j<n;j++){
-                // check if character is repeated or not
-                if(i!=j && s.charAt(i)==s.charAt(j)){
-                    found = true;
-                    break;
-                }
+        int MAX_CHAR = 26;
+        int arr[] = new int[MAX_CHAR];
+        Arrays.fill(arr,-1);
+        
+        for(int i=0;i<s.length();i++){
+            int index = s.charAt(i)-'a';
+            if(arr[index]==-1){
+                arr[index]=i;
+            }else{
+                arr[index] = -2;
             }
-            // check if found is false after second loop means there is no any repeated character
-            if(found==false){
-                return s.charAt(i); // returning the charcter
-            }
-            
         }
         
+        int idx=-1;
         
-        return '$'; // is all is repeated character then return '$'
+        for(int i=0;i<MAX_CHAR;i++){
+            if(arr[i]>=0 && (idx==-1 || arr[i]<arr[idx])){
+                idx=i;
+            }
+        }
+        
+        return (idx==-1) ? '$' : s.charAt(arr[idx]);
+        
     }
 }
